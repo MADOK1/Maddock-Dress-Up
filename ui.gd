17c98341f755a1dabba5211a1052def8e59479c9
Maddock Pearson <99259670+MADOK1@users.noreply.github.com>
2026-03-16 08:55:46 -0400

    Add new shirts, Pants, and Heads

---
+++ ui.gd
@@ -0,0 +1,40 @@
+extends Control
+var headsprite
+var shirtsprite
+var pantssprite
+ 
+func _ready() -> void:
+	headsprite = $CharacterEditor/Head
+	shirtsprite = $CharacterEditor/Shirt
+	pantssprite = $CharacterEditor/Pants
+	
+	
+func _on_white_shirt_pressed() -> void:
+	shirtsprite.texture = load("res://Shirts/WhiteShirt.png")
+
+func _on_black_shirt_pressed() -> void:
+	shirtsprite.texture = load("res://Shirts/BlackShirt.png")
+
+
+func _on_blue_pants_pressed() -> void:
+	pantssprite.texture = load("res://Pants/BluePants.png")
+
+
+func _on_green_pants_pressed() -> void:
+	pantssprite.texture = load("res://Pants/GreenPants.png")
+
+
+func _on_s_shirt_pressed() -> void:
+	shirtsprite.texture = load("res://Shirts/S-Shirt.png")
+
+
+func _on_evil_shirt_pressed() -> void:
+	shirtsprite.texture = load("res://Shirts/Evil-Shirt.png")
+
+
+func _on_smiling_pressed() -> void:
+	headsprite.texture = load("res://Heads/Smiling.png")
+
+
+func _on_normal_pressed() -> void:
+	headsprite.texture = load("res://Heads/Sadh.png")
